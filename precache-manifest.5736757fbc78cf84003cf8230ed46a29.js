284c61276c341c893d15a374f9da163c7e182bae
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-07-24 19:46:40 +1000

    Updates

---
+++ precache-manifest.5736757fbc78cf84003cf8230ed46a29.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e942a656f58b92a991685b917aca460c",
+    "url": "/ameliagoodson/Portfolio-React/index.html"
+  },
+  {
+    "revision": "709d64a38da6f25573ef",
+    "url": "/ameliagoodson/Portfolio-React/static/css/main.e7db3dee.chunk.css"
+  },
+  {
+    "revision": "36755ab7f28cf33c9909",
+    "url": "/ameliagoodson/Portfolio-React/static/js/2.14823290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ameliagoodson/Portfolio-React/static/js/2.14823290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709d64a38da6f25573ef",
+    "url": "/ameliagoodson/Portfolio-React/static/js/main.493edbc1.chunk.js"
+  },
+  {
+    "revision": "4a7423e5c0b61b6dce15",
+    "url": "/ameliagoodson/Portfolio-React/static/js/runtime-main.591dc4cb.js"
+  },
+  {
+    "revision": "4e7d605d1892cf46acf7f03a99f64a2b",
+    "url": "/ameliagoodson/Portfolio-React/static/media/port-coddiwomble2-small.4e7d605d.png"
+  },
+  {
+    "revision": "f827a5dafb33606e45d162a7f7e82bde",
+    "url": "/ameliagoodson/Portfolio-React/static/media/port-news.f827a5da.PNG"
+  },
+  {
+    "revision": "c36e138fa0f3b86b7ce1cd06ba8becb0",
+    "url": "/ameliagoodson/Portfolio-React/static/media/port-recipe-search-crop-small.c36e138f.png"
+  },
+  {
+    "revision": "c53a770e79f76cebdc5d82c05af18865",
+    "url": "/ameliagoodson/Portfolio-React/static/media/sydneyharbour-blurred-cropped.c53a770e.jpg"
+  },
+  {
+    "revision": "cb6006fb83f56f1cb318368252a50cc3",
+    "url": "/ameliagoodson/Portfolio-React/static/media/zack-woolwine-QdaZPtzpA1Q-unsplash-smaller-wide.cb6006fb.jpg"
+  }
+]);
